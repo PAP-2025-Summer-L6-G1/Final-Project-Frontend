a4be37fdd031b1b708cbe6abcbcbfba1f13e3d64
Kugusu <elias.lafornara@digitalskillsforall.org>
2025-07-30 16:45:26 -0700

    Add viewing and creating budget items

--- src/pages/Budget.jsx
+++ src/pages/Budget.jsx
@@ -3,51 +3,114 @@ import { Chart } from 'chart.js/auto'
 import "./Budget.css"
 import { useEffect, useRef } from "react";
 import AccountContext from "../contexts/AccountContext";
-import {signupUser, loginUser, logoutUser, loadLocalAccountData} from '../api/signIn.jsx'
+import { signupUser, loginUser, logoutUser, loadLocalAccountData } from '../api/signIn.jsx'
 import { useState } from "react";
+import { addBudgetItem, getBudgetItems } from "../api/budget.jsx";
 
 export default function Budget() {
-    const [loggedInUser, setLoggedInUser] = useState("");
-      useEffect(() => {
-        loadLocalAccountData(setLoggedInUser);
-      }, [])
-    const canvas = useRef("") //maybe add reduced motion options?
+
+    function handleFormSubmit(event) {
+        event.preventDefault();
+        const form = event.target;
+        let data = {
+            "ownerId": localStorage.getItem("userId"),
+            "name": form.name.value,
+            "price": form.price.value,
+            "date": new Date(),
+            "category": form.category.value
+        }
+        addBudgetItem(data);
+    }
+
+    const [loggedInUser, setLoggedInUser] = useState(""); // login code
     useEffect(() => {
-        Chart.getChart(canvas.current)?.destroy(); //https://www.chartjs.org/docs/latest/charts/doughnut.html
-        const chart = new Chart(canvas.current,
-            {
-                type: 'doughnut',
-                data: {
-                    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-                    datasets: [{
-                        label: '$',
-                        data: [12, 19, 3, 5, 2, 3],
-                    }]
-                },
-                
+        loadLocalAccountData(setLoggedInUser);
+    }, [])
+
+
+    const categoriesCanvas = useRef("") //maybe add reduced motion options?
+    const itemsCanvas = useRef("")
+    useEffect(() => {
+        async function makeCharts() { //todo: match colors for items that are in a certain category
+            const items = await getBudgetItems();
+            let categoriesData = {
+                labels: [],
+                datasets: [{
+                    label: "$",
+                    data: [],
+                    backgroundColor: ["Red", "Blue", "Green"]
+                }]
+            }
+            let itemsData = {
+                labels: [],
+                datasets: [{
+                    label: "$",
+                    data: [],
+                    backgroundColor: ["Red", "Blue", "Green"]
+                }]
+            }
+            let categories = {};
+            for (let item of items) {
+                itemsData.labels.push(item.name)
+                itemsData.datasets[0].data.push(item.price)
+                if (!categories[item.category]) {
+                    categories[item.category] = item.price;
+                }
+                else {
+                    categories[item.category] += item.price;
+                }
+            }
+            for (let category of Object.entries(categories)) {
+                categoriesData.labels.push(category[0])
+                categoriesData.datasets[0].data.push(category[1])
+            }
+            
+            Chart.getChart(categoriesCanvas.current)?.destroy(); //https://www.chartjs.org/docs/latest/charts/doughnut.html
+            const categoriesChart = new Chart(categoriesCanvas.current,
+                {
+                    type: 'doughnut',
+                    data: categoriesData,
+            }
+            );
+            Chart.getChart(itemsCanvas.current)?.destroy(); //https://www.chartjs.org/docs/latest/charts/doughnut.html
+            const itemsChart = new Chart(itemsCanvas.current,
+                {
+                    type: 'doughnut',
+                    data: itemsData,
             }
         );
-        console.log(chart)
-
+        console.log(itemsData)
+        console.log(categoriesData)
+    }
+    makeCharts();
     }, []);
     return (<>
-        <AccountContext.Provider value={{loggedInUser, setLoggedInUser, signupUser, loginUser, logoutUser}}>
-                <Navbar />
+        <AccountContext.Provider value={{ loggedInUser, setLoggedInUser, signupUser, loginUser, logoutUser }}>
+            <Navbar />
         </AccountContext.Provider>
         <main>
             <div className="pieContainer">
-                <canvas className="budget-chart"
-                    ref={canvas}
-                    >
+                <h2>Price by Category</h2>
+                <canvas className="categories-chart"
+                    ref={categoriesCanvas}
+                >
                     The pie chart failed to render. Please check if there is anything preventing canvases from working on your device.
                 </canvas>
             </div>
-            <form>
+            <div className="pieContainer">
+                <h2>Price by Item</h2>
+                <canvas className="items-chart"
+                    ref={itemsCanvas}
+                    >
+                    The pie chart failed to render. Please check if there is anything preventing canvases from working on your device.
+                </canvas>
+                    </div>
+            <form onSubmit={handleFormSubmit}>
                 <label htmlFor="name">Item name:</label>
                 <input type="text" id="name" />
-                <label htmlFor="price"></label>
-                <input type="text" id="price" />
-                <label htmlFor="category"></label>
+                <label htmlFor="price">Price:</label>
+                <input type="number" id="price" />
+                <label htmlFor="category">Category</label>
                 <input type="text" id="category" />
                 <button type="submit">Submit</button>
             </form>
