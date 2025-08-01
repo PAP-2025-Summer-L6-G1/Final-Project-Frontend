23fd3c375ca1338c6195b1d191ed2bd485f73c5b
sammygiang24 <sammygiang24@gmail.com>
2025-07-31 17:23:26 -0700

    Implemented editing, Categories, fixed up CSS a bit

--- src/components/GroceryList.jsx
+++ src/components/GroceryList.jsx
@@ -1,38 +1,13 @@
 import { useEffect, useContext, useState } from "react";
 import "./GroceryList.css"
 import GroceryContext from "../contexts/GroceryContext";
-import AccountContext from "../contexts/AccountContext";
-import { useRef } from "react";
+import GroceryListItem from "./GroceryListItem";
+import GroceryListForm from "./GroceryListForm";
 
 export default function GroceryList() {
     const groceryContext = useContext(GroceryContext);
-    const accountContext = useContext(AccountContext);
-    
-    const [name, setName] = useState("");
-    const [quantity, setQuantity] = useState(1);
-    const [category, setCategory] = useState("dairy");
-    const [isBought, setIsBought] = useState(false);
 
-    async function handleAddItem(event) {
-        event.preventDefault();
-        const success = await groceryContext.newItem({
-            ownerId: accountContext.loggedInUser,
-            name: name, 
-            quantity: quantity,
-            category: category,
-            isBought: isBought,
-            storageType: "list"
-        }, groceryContext.items, groceryContext.setItems);
-
-        if (success) {
-            setName("");
-            setQuantity(1);
-            setCategory("");
-            setIsBought(false);
-        } 
-
-        await groceryContext.getItems(accountContext.loggedInUser, groceryContext.setItems);
-    }
+    const [filterCategory, setFilterCategory] = useState("");
 
     async function sendAllBoughtToBag() {
         const checkedItem = groceryContext.items.filter((item) => item.storageType === "list" && item.isBought)
@@ -40,60 +15,52 @@ export default function GroceryList() {
         for (let item of checkedItem) {
             await groceryContext.updateStorageType(item._id, "bag", groceryContext.items, groceryContext.setItems);
         }
-
     }
-    
 
+    const allCategories = ["dairy", "meat", "grain", "fruit"];
+
+    const filteredItems = groceryContext.items.filter((item) => (
+        item.storageType === "list" && (item.category === filterCategory || filterCategory === "")
+    ))
+
+    const groupedItems = {};
+    filteredItems.forEach((item) => {
+        const category = item.category;
+        if (!groupedItems[category]) {
+            groupedItems[category] = [];
+        }
+        groupedItems[category].push(item);
+    })
+    
     return (
-        <div className="grocery-list">
-            <h2> Grocery list </h2>
-            <form id="add-item-form" onSubmit={handleAddItem}>
-                <label>Owner: {accountContext.loggedInUser}</label>
-    
-                <label htmlFor="item-name">Item Name:</label>
-                <input
-                    type="text"
-                    id="item-name"
-                    value={name}
-                    onChange={(event)=> setName(event.target.value)}
-                    required />
-                <label htmlFor="item-quantity">Quantity:</label>
-                <input
-                    type="number"
-                    id="item-quantity"
-                    value={quantity}
-                    onChange={(event) => setQuantity(event.target.value)}
-                    required />
-                <label htmlFor="item-category">Category:</label>
-                <select
-                    id="item-category"
-                    value={category}
-                    onChange={(event) => setCategory(event.target.value)}
-                    required>
+        <div className="grocery-list-page-container">
+            <div className="grocery-list">
+                <h2> Grocery list </h2>
+                <div className="filter">
+                    <label htmlFor="category-filter">Filter by category:</label>
+                    <select id="category-filter" value={filterCategory} onChange={(event) => setFilterCategory(event.target.value)} >
+                        <option value="">All Categories</option>
+                        {allCategories.map((category) => (
+                            <option key={category} value={category}>{category}</option>
+                        ))}
+                    </select>
+                </div>
+                {filteredItems.length === 0 ? (<p>No items in list for selected category.</p>) : null}
+                {Object.entries(groupedItems).map(([category, items]) => (
+                    <div className="category-group">
+                        <h3 className="category-label">{category}</h3>
+                        {items.map((item, index) => (
+                            <GroceryListItem key={index} item={item} />
+                        ))}
+                    </div>
+                ))}
 
-                    <option value="dairy">Dairy</option>
-                    <option value="meat">Meat</option>
-                    <option value="fruit">Fruit</option>
-                    <option value="other">Other</option>
-                </select>
-                <button type="submit">Add Item</button>
-                <ul>
-                    {groceryContext.items.filter((item) => item.storageType === "list").map((item, index) => (
-                        <li key={index}>
-                            <input
-                                type="checkbox"
-                                checked={item.isBought}
-                                onChange={() => groceryContext.updateIsBought(item._id, !item.isBought, groceryContext.items, groceryContext.setItems)}
-                            />
-                            Name: {item.name}, 
-                            Quantity: {item.quantity}, 
-                            Category: {item.category}, 
-                            IsBought: {item.isBought ? "yes" : "no"}  
-                        </li>
-                    ))}
-                </ul>
-                <button type="button" onClick={sendAllBoughtToBag}>Send bought to bag</button>
-            </form>
+                <button type="button" className="send-bag-button" onClick={sendAllBoughtToBag}>Send bought to bag</button>
+            </div>
+
+            <div className="grocery-list-form">
+                <GroceryListForm />
+            </div>
         </div>
     )
 }
