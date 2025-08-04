b2494ecb5255d3a35966dc5f800160b2c6c6ca0d
Kugusu <elias.lafornara@digitalskillsforall.org>
2025-08-04 16:26:09 -0700

    Removed strict mode from budget tracker to fix bug

--- src/main.jsx
+++ src/main.jsx
@@ -2,40 +2,48 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Home from './pages/Home.jsx'
-import {createBrowserRouter, RouterProvider} from "react-router-dom"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import Budget from './pages/budget.jsx'
 
 
 const router = createBrowserRouter([
   {
-  path: "/",
-  element: <Home />,
+    path: "/",
+    element: <StrictMode>
+      <Home />
+    </StrictMode>,
   },
   {
-  path: "/inventory",
-  element: <Home />,
+    path: "/inventory",
+    element: <StrictMode>
+      <Home />
+    </StrictMode>,
   },
   {
-  path: "/grocery",
-  element: <Home />,
+    path: "/grocery",
+    element: <StrictMode>
+      <Home />
+    </StrictMode>,
   },
   {
-  path: "/health",
-  element: <Home />,
+    path: "/health",
+    element: <StrictMode>
+      <Home />
+    </StrictMode>,
   },
   {
-  path: "/recipes",
-  element: <Home />,
+    path: "/recipes",
+    element: <StrictMode>
+      <Home />
+    </StrictMode>,
   },
   {
-  path: "/budget",
-  element: <Budget />,
+    path: "/budget",
+    element: <Budget />,
   },
 ])
 
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
+  <RouterProvider router={router} />
 )
