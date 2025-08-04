bd975f3d7e349d0c70811f74c8bc6ba98fafb97c
Kugusu <elias.lafornara@digitalskillsforall.org>
2025-08-04 13:05:09 -0700

    Non-functioning chart code

--- src/pages/Budget.jsx
+++ src/pages/Budget.jsx
@@ -1,5 +1,5 @@
 import Navbar from "../components/Navbar";
-import { Chart } from 'chart.js/auto'
+import { Chart, ArcElement, Tooltip, Legend, DoughnutController, Title } from 'chart.js'
 import "./Budget.css"
 import { useEffect, useRef } from "react";
 import AccountContext from "../contexts/AccountContext";
@@ -7,82 +7,143 @@ import { signupUser, loginUser, logoutUser, loadLocalAccountData } from '../api/
 import { useState } from "react";
 import { addBudgetItem, getBudgetItems } from "../api/budget.jsx";
 
+Chart.register(ArcElement, Tooltip, Legend, DoughnutController, Title);
+
 export default function Budget() {
-
-    function handleFormSubmit(event) {
-        event.preventDefault();
-        const form = event.target;
-        let data = {
-            "ownerId": localStorage.getItem("userId"),
-            "name": form.name.value,
-            "price": form.price.value,
-            "date": new Date(),
-            "category": form.category.value
-        }
-        addBudgetItem(data);
-    }
-
     const [loggedInUser, setLoggedInUser] = useState(""); // login code
     useEffect(() => {
         loadLocalAccountData(setLoggedInUser);
     }, [])
 
+    const [categoriesDatapoints, setCategoriesDatapoints] = useState([])
+    const [itemsDatapoints, setItemsDatapoints] = useState([])
+    
+    let categoriesData = {
+        labels: [],
+        datasets: [{
+            label: "$",
+            data: [],
+            backgroundColor: ["red", "blue", "green"]
 
+        }]
+    }
+    let itemsData = {
+        labels: [],
+        datasets: [{
+            label: "$",
+            data: [],
+            backgroundColor: ["red", "blue", "green"]
+        }]
+    }
     const categoriesCanvas = useRef("") //maybe add reduced motion options?
     const itemsCanvas = useRef("")
-    useEffect(() => {
-        async function makeCharts() { //todo: match colors for items that are in a certain category
-            const items = await getBudgetItems();
-            let categoriesData = {
-                labels: [],
-                datasets: [{
-                    label: "$",
-                    data: [],
 
-                }]
-            }
-            let itemsData = {
-                labels: [],
-                datasets: [{
-                    label: "$",
-                    data: [],
-
-                }]
-            }
-            let categories = {};
-            for (let item of items) {
-                itemsData.labels.push(item.name)
-                itemsData.datasets[0].data.push(item.price)
-                if (!categories[item.category]) {
-                    categories[item.category] = item.price;
-                }
-                else {
-                    categories[item.category] += item.price;
-                }
-            }
-            for (let category of Object.entries(categories)) {
-                categoriesData.labels.push(category[0])
-                categoriesData.datasets[0].data.push(category[1])
-            }
-
-            Chart.getChart(categoriesCanvas.current)?.destroy(); // make chart for categories
-            const categoriesChart = new Chart(categoriesCanvas.current,
-                {
-                    type: 'doughnut',
-                    data: categoriesData,
-                }
-            );
-            Chart.getChart(itemsCanvas.current)?.destroy(); //make chart for items
-            const itemsChart = new Chart(itemsCanvas.current,
-                {
-                    type: 'doughnut',
-                    data: itemsData,
-                }
-            );
-
+    async function handleFormSubmit(event) {
+        event.preventDefault();
+        const form = event.target;
+        let data = {
+            ownerId: localStorage.getItem("userId"),
+            name: form.name.value,
+            price: Number(form.price.value),
+            date: new Date(),
+            category: form.category.value
         }
-        makeCharts();
+        let result = await addBudgetItem(data);
+        if (result) { // if the item was successfully added, add it to the chart data
+            addItemsToChartData([data])
+        }
+    }
+
+    function addItemsToChartData(items) {
+        let categories = {};
+        for (let item of items) {
+            if (itemsData.labels.indexOf(item.name) == -1) { // if there is no item with the given name, add it to the data
+                itemsData.labels.push(item.name)
+                setItemsDatapoints(prevDatapoints => [...prevDatapoints, item.price]) // set itemDatapoints to itemDatapoints with item.price at the end 
+            }
+
+            if (!categories[item.category]) { // if the category is not already in categories, create it and set the category's value to the item price
+                categories[item.category] = item.price;
+            }
+            else { // otherwise, add the item's price to the category's value
+                categories[item.category] += item.price;
+            }
+        }
+        for (let category of Object.entries(categories)) {
+            let indexOfCategory = categoriesData.labels.indexOf(category[0]);
+            if (indexOfCategory == -1) { // if the category does not already exist in the labels, push it 
+                categoriesData.labels.push(category[0])
+                setCategoriesDatapoints(prevDatapoints => [...prevDatapoints, category[1]]) // set categoriesDatapoints to itemDatapoints with the at the end 
+            }
+            else {
+                setCategoriesDatapoints(categoriesDatapoints.map((index) => index == indexOfCategory ? categories[1] : categoriesDatapoints[index]))
+            }
+        }
+    }
+
+
+    async function getItemsAndAddToData() {
+        try {// get the budget items from the current user and send them to the addItemsToChartData method
+            const items = await getBudgetItems();
+            setItemsDatapoints([])
+            setCategoriesDatapoints([])
+            addItemsToChartData(items);
+        }
+        catch (error) {
+            console.error(error);
+        }
+    }
+    function makeCharts() { //todo: match colors for items that are in a certain category
+        Chart.getChart(categoriesCanvas.current)?.destroy(); // delete the chart from the canvas if there is already one
+        // make chart for categories
+        const categoriesChart = new Chart(categoriesCanvas.current,
+            {
+                type: 'doughnut',
+                data: categoriesData,
+                options: {
+                    plugins: {
+                        title: {
+                            display: true,
+                            text: "Price by Category"
+                        }
+                    }
+                }
+            }
+        );
+
+        Chart.getChart(itemsCanvas.current)?.destroy(); // delete the chart from the canvas if there is already one
+        //make chart for items
+        const itemsChart = new Chart(itemsCanvas.current,
+            {
+                type: 'doughnut',
+                data: itemsData,
+                options: {
+                    plugins: {
+                        title: {
+                            display: true,
+                            text: "Price by Item"
+                        }
+                    }
+                }
+            }
+        );
+    }
+
+    useEffect(() => {
+        async function asyncWrapper() {
+            await getItemsAndAddToData()
+        }
+        asyncWrapper()
+
     }, []);
+    useEffect(makeCharts, [])
+    useEffect(() => {
+        categoriesData.datasets.data = categoriesDatapoints
+        itemsData.datasets.data = itemsDatapoints
+        Chart.getChart(categoriesCanvas.current)?.update()
+        Chart.getChart(itemsCanvas.current)?.update()
+        console.log(itemsData)
+    }, [categoriesDatapoints, itemsDatapoints])
     return (<>
         <AccountContext.Provider value={{ loggedInUser, setLoggedInUser, signupUser, loginUser, logoutUser }}>
             <Navbar />
@@ -90,7 +151,6 @@ export default function Budget() {
         <main>
             <div className="gridContainer">
                 <div className="pieContainer cell1">
-                    <h2>Price by Category</h2>
                     <canvas className="categories-chart"
                         ref={categoriesCanvas}
                     >
@@ -98,7 +158,6 @@ export default function Budget() {
                     </canvas>
                 </div>
                 <div className="pieContainer cell2">
-                    <h2>Price by Item</h2>
                     <canvas className="items-chart"
                         ref={itemsCanvas}
                     >
@@ -111,7 +170,7 @@ export default function Budget() {
                     <label htmlFor="price">Price:</label>
                     <input type="number" id="price" />
                     <label htmlFor="category">Category</label>
-                    <select id="category">
+                    <select id="category" required>
                         <option value="Food">Food</option>
                         <option value="Housing">Housing</option>
                         <option value="Utilities">Utilities</option>
@@ -121,6 +180,7 @@ export default function Budget() {
                     <button type="submit">Submit</button>
                 </form>
             </div>
+            <button onClick={getItemsAndAddToData}></button>
         </main>
         <script>
 
