05ef9feb495c508cb1965a07464675a51ff84109
Kugusu <elias.lafornara@digitalskillsforall.org>
2025-08-06 13:45:37 -0700

    Fix Item creation bug and add deleting items

--- src/pages/Budget.jsx
+++ src/pages/Budget.jsx
@@ -5,37 +5,29 @@ import { useEffect, useRef } from "react";
 import AccountContext from "../contexts/AccountContext";
 import { signupUser, loginUser, logoutUser, loadLocalAccountData } from '../api/signIn.jsx'
 import { useState } from "react";
-import { addBudgetItem, getBudgetItems } from "../api/budget.jsx";
+import { addBudgetItem, deleteBudgetItem, getBudgetItems } from "../api/budget.jsx";
 
-Chart.register(ArcElement, Tooltip, DoughnutController);
+Chart.register(ArcElement, Tooltip, DoughnutController); // These are the elements of the chart that are used. There are others that could be used, but I did not implement them.
 
+function colorGenerator(context) { // match a color with the name of an item's category or with a category's name
+    const object = context.raw;
+    if (!object) {
+        return 0;
+    }
+    const categoryNames = ["Entertainment", "Food", "Housing", "Transportation", "Utilities"] // For choosing colors
+    const colors = ["red", "blue", "green", "orange", "purple"] // Each color corresponds to the category with the same index
+    const color = colors[categoryNames.indexOf(object.category)];
+    return color;
+}
 
 let chartData = {
     datasets: [{
         data: [],
-        backgroundColor: (context) => {
-            const item = context.raw;
-            if (!item) {
-                return 0;
-            }
-            const colors = ["red", "blue", "green", "orange", "purple"]
-            const categoryNames = ["Entertainment", "Food", "Housing", "Transportation", "Utilities"] // for choosing colors
-            const color = colors[categoryNames.indexOf(item.category)];
-            return color;
-        }
+        backgroundColor: colorGenerator
     },
     {
         data: [],
-        backgroundColor: (context) => {
-            const item = context.raw;
-            if (!item) {
-                return 0;
-            }
-            const colors = ["red", "blue", "green", "orange", "purple"]
-            const categoryNames = ["Entertainment", "Food", "Housing", "Transportation", "Utilities"] // for choosing colors
-            const color = colors[categoryNames.indexOf(item.category)];
-            return color;
-        }
+        backgroundColor: colorGenerator
     }]
 }
 export default function Budget() {
@@ -43,8 +35,6 @@ export default function Budget() {
     useEffect(() => {
         loadLocalAccountData(setLoggedInUser);
     }, [])
-
-    const [counter, setCounter] = useState(0); // increase to trigger an update
 
     const chartCanvas = useRef("") //maybe add reduced motion options?
 
@@ -58,24 +48,25 @@ export default function Budget() {
             date: new Date(),
             category: form.category.value
         }
-        let result = await addBudgetItem(data);
+        let result = await addBudgetItem(data); // Add the item to mongoDB
         if (result) { // if the item was successfully added, add it to the chart data
-            addItemsToChartData([data])
+            addItemsToChartData([data]);
         }
     }
-    
-    function addItemsToChartData(items, clearData=false) {
-        if (clearData == true) {
-            chartData.datasets[1].data = []
-            chartData.datasets[0].data = []
+
+    function addItemsToChartData(items, clearData = false) {
+        if (clearData == true) { // clear out the datasets. I don't think this is ever actually useful, because clearData is only true in
+            chartData.datasets[1].data = [] // getItemsAndAddToData, which is only called on the first load of the page,
+            chartData.datasets[0].data = [] // but it might be in the future
 
         }
         let categories = [];
         chartData.datasets[1].data = [...chartData.datasets[1].data, ...items] // append items to the data
         for (let item of items) {
             let indexOfCategory = categories.findIndex((element) => element.category == item.category)
+            console.log(indexOfCategory, item.category)
             if (indexOfCategory == -1) { // if the category is not already in chart, create it and set the category's value to the item price
-                categories.push({category: item.category, price: item.price}); 
+                categories.push({ category: item.category, price: item.price });
             }
             else { // otherwise, add the item's price to the category's value
                 categories[indexOfCategory].price += item.price;
@@ -83,16 +74,17 @@ export default function Budget() {
         }
         for (let category of categories) {
             let indexOfCategory = chartData.datasets[0].data.findIndex((element) => element.category == category.category); // index of category in data
+            console.log(indexOfCategory, category.category)
             if (indexOfCategory == -1) { // if the category does not already exist in the labels, push it 
                 chartData.datasets[0].data.push(category) // set chartDatapoints to itemDatapoints with the at the end 
             }
             else {
-                chartData.datasets[0].data[indexOfCategory] += category.price
+                chartData.datasets[0].data[indexOfCategory].price += category.price
             }
         }
         chartData.datasets[1].data.sort((a, b) => a.category.localeCompare(b.category))
         chartData.datasets[0].data.sort((a, b) => a.category.localeCompare(b.category))
-        setCounter(prevCount => prevCount + 1) // only used to make the page update
+        Chart.getChart(chartCanvas.current).update();
     }
 
 
@@ -107,7 +99,7 @@ export default function Budget() {
     }
     function makeCharts() { //todo: match colors for items that are in a certain category
         Chart.getChart(chartCanvas.current)?.destroy(); // delete the chart from the canvas if there is already one
-        // make chart for chart
+        // Make the chart
         const chartChart = new Chart(chartCanvas.current,
             {
                 type: 'doughnut',
@@ -121,23 +113,22 @@ export default function Budget() {
                         tooltip: {
                             callbacks: {
                                 title: (context) => {
-                                    const item = context[0].raw;
-                                        if (!item) {
-                                            return "";
-                                        }
-                                        if (context[0].datasetIndex == 0) {
-                                            return item.category;
-                                        }
-                                        if (context[0].datasetIndex == 1) {
-                                            return item.name;
-                                        }
-                                        else {
-                                            return "";
-                                        }
+                                    const object = context[0].raw;
+                                    if (!object) {
+                                        return "";
+                                    }
+                                    if (context[0].datasetIndex == 0) { // If the object is a category, return the category name
+                                        return object.category;
+                                    }
+                                    else if (context[0].datasetIndex == 1) { // If the object is an item, return the item name
+                                        return object.name;
+                                    }
+                                    else {
+                                        return "";
+                                    }
                                 },
                                 label: (context) => {
-                                    return `\$${context.parsed}`
-
+                                    return `\$${context.parsed}`; // return the parsed value (price) with a $ in front of it
                                 }
                             }
                         }
@@ -158,9 +149,7 @@ export default function Budget() {
         asyncWrapper()
 
     }, []);
-    useEffect(() => {
-        Chart.getChart(chartCanvas.current).update()
-    }, [counter])
+
     return (<>
         <AccountContext.Provider value={{ loggedInUser, setLoggedInUser, signupUser, loginUser, logoutUser }}>
             <Navbar />
@@ -170,17 +159,53 @@ export default function Budget() {
                 <div className="pieContainer">
                     <canvas className="chart"
                         ref={chartCanvas}
+                        onClick={async (event) => {
+                            //https://masteringjs.io/tutorials/chartjs/onclick-bar-chart
+                            const clickedObjects = Chart.getChart(event.target).getElementsAtEventForMode(event, "nearest", { intersect: true }, true);
+                            let categories = [];
+                            let numDeleted = 0;
+                            for (let object of clickedObjects) {
+                                if (object.datasetIndex == 1) {
+                                    const actualIndex = object.index - numDeleted;
+                                    const item = chartData.datasets[object.datasetIndex].data[actualIndex];
+                                    const result = await deleteBudgetItem(item._id)
+                                    if (result) { // if the item was successfully deleted, add it to an object in the categories array that is the category name and the price of all the object in that category that were removed
+                                        const indexOfCategory = categories.findIndex((element) => element.category == item.category)
+
+                                        if (indexOfCategory == -1) { // make a list of the amount of money getting removed from the each category 
+                                            categories.push({ category: item.category, price: item.price });
+                                        }
+                                        else {
+                                            categories[indexOfCategory].price += item.price;
+                                        }
+
+                                        chartData.datasets[object.datasetIndex].data.splice(actualIndex, 1); // remove the item from the data
+                                        numDeleted++;
+                                    }
+                                }
+                            }
+                            for (let category of categories) {
+                                const indexOfCategory = chartData.datasets[0].data.findIndex((element) => element.category == category.category); // index of category in data
+                                console.log(indexOfCategory, category.category)
+                                if (indexOfCategory != -1) { // if the category is in the data, 
+                                    chartData.datasets[0].data[indexOfCategory].price -= category.price // subtract the price of the deleted items from it
+                                }
+                            }
+                            Chart.getChart(chartCanvas.current).update();
+                        }}
                     >
                         The pie chart failed to render. Please check if there is anything preventing canvases from working on your device.
                     </canvas>
+                    <p>Click on an item to delete it.</p>
                 </div>
                 <form onSubmit={handleFormSubmit} id="budgetForm">
                     <label htmlFor="name">Item name:</label>
                     <input type="text" id="name" />
                     <label htmlFor="price">Price:</label>
-                    <input type="number" id="price" step=".01" min="0" inputMode="decimal"/>
+                    <input type="number" id="price" step=".01" min="0" inputMode="decimal" />
                     <label htmlFor="category">Category</label>
                     <select id="category" required>
+                        {/* To add another category, add an option here, ideally in alphabetical order, and then add it to categoryNames in colorGenerator and a correctponding color to colors in colorGenerator  */}
                         <option value="Entertainment">Entertainment</option>
                         <option value="Food">Food</option>
                         <option value="Housing">Housing</option>
