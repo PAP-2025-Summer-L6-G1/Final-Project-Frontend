63ee3bdd60ae17771ec4ea10ffd5fa4cf96f99a9
Rongrui Zhu <ronrzhu@gmail.com>
2025-08-07 16:37:35 -0700

    Updated homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,8 @@ import AccountContext from '../contexts/AccountContext.jsx'
 import {signupUser, loginUser, logoutUser, loadLocalAccountData} from '../api/signIn.jsx'
 import { useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import freezerIcon from "../assets/Freezer.svg"
+import groceryScene from "../assets/grocery.jpg"
 function Home() {
   const [loggedInUser, setLoggedInUser] = useState("");
   useEffect(() => {
@@ -16,28 +18,46 @@ function Home() {
         <Navbar />
       </AccountContext.Provider>
       <main>
+        <img id="homepage-background" src={groceryScene} />
         <div className='card-container'>
-
-          <div className='card'>
+          <div className='homepage-card'>
+            <img src={freezerIcon} className="homepage-card-icon" />
+            <div className="homepage-card-text">
               <Link to="/inventory"><h2>Inventory</h2></Link>
-              <p>Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing Testing </p>
+              <p>Description</p>
+            </div>
           </div>
-          <div className='card'>
-              <Link to="/grocery"><h2>Grocery List</h2></Link>
-              <p></p>
-          </div>
-          <div className='card'>
-              <Link to="/health"><h2>Health</h2></Link>
-              <p></p>
-          </div>
-          <div className='card'>
-              <Link to="/recipes"><h2>Recipes</h2></Link>
-              <p></p>
-          </div>
-          <div className='card'>
 
+          <div className='homepage-card'>
+            <img src={freezerIcon} className="homepage-card-icon" />
+            <div className="homepage-card-text">
+              <Link to="/grocery"><h2>Grocery List</h2></Link>
+              <p>Description</p>
+            </div>
+          </div>
+
+          <div className='homepage-card'>
+            <img src={freezerIcon} className="homepage-card-icon" />
+            <div className="homepage-card-text">
+              <Link to="/health"><h2>Health</h2></Link>
+              <p>Description</p>
+            </div>
+          </div>
+
+          <div className='homepage-card'>
+            <img src={freezerIcon} className="homepage-card-icon" />
+            <div className="homepage-card-text">
+              <Link to="/recipes"><h2>Recipes</h2></Link>
+              <p>Description</p>
+            </div>
+          </div>
+
+          <div className='homepage-card'>
+            <img src={freezerIcon} className="homepage-card-icon" />
+            <div className="homepage-card-text">
               <Link to="/budget"><h2>Budget Tracker</h2></Link>
-              <p></p>
+              <p>Description</p>
+            </div>
           </div>
         </div>
       </main>
