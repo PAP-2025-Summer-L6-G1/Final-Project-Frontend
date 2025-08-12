0fe5d0135446bac31947757915210662259bba72
vinnyly <vincently06@gmail.com>
2025-08-12 16:28:35 -0700

    done with styling and triggering search when adding/removing ingred.

--- src/pages/SavedRecipe.jsx
+++ src/pages/SavedRecipe.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import Navbar from '../components/Navbar.jsx'
 import RecipeCard from '../components/RecipeCard.jsx'
 import { useSavedRecipes } from '../hooks/useSavedRecipes.js';
+import './SavedRecipe.css'
 
 function SavedRecipe() {
     const { SavedRecipes, fetchSavedRecipes, saveRecipe, unsaveRecipe, checkIfSaved } = useSavedRecipes();
@@ -11,7 +12,10 @@ function SavedRecipe() {
         <>
             <Navbar/>
             <main>
-                <Link to="/recipes"><button>Back to recipes</button></Link>
+                <div className='saved-recipes-header'>
+                    <h1 class="saved-recipes-title">Saved Recipes</h1>
+                    <Link to="/recipes"><button id='recipes-btn'>Back to recipes ⇨</button></Link>
+                </div>
                 {SavedRecipes.map((recipe) => {
                     return (
                         <RecipeCard
@@ -27,7 +31,9 @@ function SavedRecipe() {
                             vegetarian={recipe.vegetarian}
                             vegan={recipe.vegan}
                             glutenFree={recipe.glutenFree}
-                            dairyFree={recipe.dairyFree} />
+                            dairyFree={recipe.dairyFree}
+                            sum={recipe.summary}
+                        />
                     )
                 }
                 )}
